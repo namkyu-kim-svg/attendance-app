3a30b1b857af99beb5ba0521de19a66ee5098f51
namkyu-kim-svg <neb.nkkim@gmail.com>
2025-05-21 15:55:43 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AttendanceApp from './AttendanceApp';
+
+function App() {
+  return (
+    <div className="App">
+      <AttendanceApp />
+    </div>
+  );
+}
+
+export default App;
